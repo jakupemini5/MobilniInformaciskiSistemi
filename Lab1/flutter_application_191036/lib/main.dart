f7bffac3a9fbb69f5293c4bcb13c2675904b9d04
Jakup Emini <jakup.emini.5@gmail.com>
2024-11-29 17:56:00 +0100

    improved cards and layout

--- Lab1/flutter_application_191036/lib/main.dart
+++ Lab1/flutter_application_191036/lib/main.dart
@@ -1,59 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_191036/widgets/ClothingItemCard.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class ClothingItemCard extends StatelessWidget {
-  final int price;
-  final String name;
-  final String imageUrl;
-
-  const ClothingItemCard({
-    super.key,
-    required this.price,
-    required this.name,
-    required this.imageUrl,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey.shade400, // Adjust as needed
-      elevation: 12,
-      shadowColor: Colors.blue,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Display image
-            Expanded(
-              child: Image.network(
-                imageUrl,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) =>
-                    const Icon(Icons.error, size: 50, color: Colors.black),
-              ),
-            ),
-            const SizedBox(height: 8),
-            // Display name
-            Text(
-              name,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            // Display price
-            Text(
-              "\$${price.toString()}",
-              style: const TextStyle(fontSize: 14, color: Colors.white),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
 
 
@@ -66,21 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Jakup Emini 191036',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor:  const Color.fromARGB(255, 0, 167, 251)),
         useMaterial3: true,
       ),
@@ -91,15 +25,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -112,46 +37,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.s
-        backgroundColor: Colors.grey.shade900,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Color.fromARGB(255, 28, 28, 28),
         title: Text(widget.title, style: const TextStyle(color: Colors.white70, fontWeight: FontWeight.bold),),
       ),
       body:   GridView.count(
         crossAxisCount: 2,
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
         children: const <Widget>[
-          ClothingItemCard(price: 10, name: 'hello world', imageUrl: 'https://picsum.photos/seed/123/400/300',),
-          ClothingItemCard(price: 10, name: 'hello world', imageUrl: 'https://picsum.photos/seed/456/400/300',),
-          ClothingItemCard(price: 10, name: 'hello world', imageUrl: 'https://picsum.photos/seed/789/400/300',)
+          ClothingItemCard(price: 10, name: 'Red Jacket', imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',),
+          ClothingItemCard(price: 10, name: 'Item 2', imageUrl: 'https://picsum.photos/seed/b/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 3', imageUrl: 'https://picsum.photos/seed/c/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 4', imageUrl: 'https://picsum.photos/seed/d/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 5', imageUrl: 'https://picsum.photos/seed/e/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 6', imageUrl: 'https://picsum.photos/seed/f/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 7', imageUrl: 'https://picsum.photos/seed/g/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 8', imageUrl: 'https://picsum.photos/seed/h/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 9', imageUrl: 'https://picsum.photos/seed/i/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 10', imageUrl: 'https://picsum.photos/seed/j/400/300',),
+          ClothingItemCard(price: 10, name: 'Item 11', imageUrl: 'https://picsum.photos/seed/k/400/300',),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
